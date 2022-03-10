066c27ecc2ff40e2af8c17a7f1f17ad65c1866ab
mohamad <mohamadfarbodwd@gmail.com>
2022-03-10 15:47:50 +0330

    AllProducts component implented

--- components/products/AllProducts.js
+++ components/products/AllProducts.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AllProducts = () => {
-  return <div>AllProducts</div>
+const AllProducts = ({ onProductIdSubmit }) => {
+  const [productId, setProductId] = useState('')
+
+  return (
+    <div className="mx-auto my-8 w-full">
+      <label htmlFor="productId" className="mx-2">
+        {' '}
+        Please insert Your Product ID
+      </label>
+      <input
+        className="mx-2 border border-gray-600"
+        name="productId"
+        type="text"
+        value={productId}
+        onChange={(e) => {
+          setProductId(e.target.value)
+        }}
+      />
+      <button
+        onClick={() => onProductIdSubmit(productId)}
+        className="rounded-md bg-gray-800 px-2 py-1 text-white"
+      >
+        submit
+      </button>
+    </div>
+  )
 }
 
 export default AllProducts
